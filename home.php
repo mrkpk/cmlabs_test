367f0126536e3be788d522689bbb3be9785ef44c
Willy <willychristian11@gmail.com>
2022-03-17 23:53:58 +0700

    adding files

---
+++ home.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width" , intial-scale="1.0">
+    <title>CMLABS - Test</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+
+    <div class="row">
+        <div class="col-1">
+            <h1>Latest<br> Design Blog</h1>
+            <h4>Lorem ipsum is simply dummy text of the printing and typesetting</h4>
+            <h4>Lorem ipsum has been the industry's standard dummy</h4>
+            <br>
+
+            <button type="button">Learn more</button>
+
+        </div>
+        <div class="color-box">
+        </div>
+
+        <div class="col-2">
+
+            <img src="meeting.jpg" class="foto">
+        </div>
+    </div>
+</body>
+
+</html>
